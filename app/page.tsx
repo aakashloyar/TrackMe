52bc971f9174daaddeadf6a562f36a7e64675910
aakashxyz-54 <aakash.loyar@komprise.com>
2025-07-28 20:37:51 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
+  console.log("*******", process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY);
   return (
     <div>
       { process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}
